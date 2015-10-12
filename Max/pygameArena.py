706f9e8e34757abe7ddf6b95226e897fdfa241a2
Max <max@thetroll.org.uk>
2015-10-12 19:49:49 +0100

    #3 pygameArena.py Pygame display for an arena. No colour.

---
+++ Max/pygameArena.py
@@ -0,0 +1,31 @@
+from suds.client import Client
+import json
+import robotArena
+import arenaMonitor
+import sys
+sys.path.append('../Dave/PyGame')
+from PyGameBase import PyGameBase
+import pygame
+
+class PyGameArena(PyGameBase):
+    def __init__(self,width,height,frameRate,arena):
+        pygame.init()
+        self.surface = pygame.display.set_mode((width,height))
+        self.frameRate = frameRate
+        self.arena = arena
+    def Update(self,deltaT):
+        self.arena.getRobots()
+    def Render(self,deltaT):
+        self.Clear()
+        for robot in self.arena.robots:
+            X = self.arena.getRobotState(robot).X*10+100
+            Y = 200-self.arena.getRobotState(robot).Y*10
+            pygame.draw.ellipse(self.surface,(255,255,255),(X,Y,20,20))
+
+url = "http://192.168.0.14:49711/Arena.asmx?WSDL"
+arena = int(input("Arena handle? "))
+thisArena = arenaMonitor.Arena(url,arena)
+print("thisArena done")
+thisPyGame = PyGameArena(400,400,40,thisArena)
+print("thisPyGame done")
+thisPyGame.Start()
